ec89af84ad324f961a62d3d9b07fe9be7db10aa8
KARAN.BATRA <KARAN.BATRA>
2016-06-27 12:38:07 +0530

    Add list_view for seeNotes

--- app/src/main/java/com/example/karanbatra/productiveninja/activity/CreateNote.java
+++ app/src/main/java/com/example/karanbatra/productiveninja/activity/CreateNote.java
@@ -1,21 +1,12 @@
 package com.example.karanbatra.productiveninja.activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.karanbatra.productiveninja.R;
-import com.example.karanbatra.productiveninja.activity.AnalyticsFragment;
-import com.example.karanbatra.productiveninja.activity.NotesDB;
-import com.example.karanbatra.productiveninja.activity.NotesFields;
-
-import java.util.List;
 
 public class CreateNote extends AppCompatActivity {
 
@@ -26,7 +17,6 @@ public class CreateNote extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_note);
-        Log.e("---------","asfdfsa");
 
         saves=(Button) findViewById(R.id.savebutton);
         saves.setOnClickListener(new View.OnClickListener() {
@@ -39,11 +29,7 @@ public class CreateNote extends AppCompatActivity {
                 final String contentstr=contents.getText().toString();
 
                 db.addContact(new NotesFields(titlestr,contentstr));
-                List<NotesFields> contacts = db.getAllContacts();
-                for (NotesFields cn : contacts) {
-                    String log = "Id: " + cn.get_id() + " ,Name: " + cn.get_name()   +", contents: "+cn.get_contents() + " deletevar :" + cn.get_delelte_var() + "\n";
-                    Log.e("hhh", log);
-                }
+
                 Intent intent = new Intent(v.getContext(), MainActivity.class);
                 startActivity(intent);
             }
--- app/src/main/java/com/example/karanbatra/productiveninja/activity/SeeNotes.java
+++ app/src/main/java/com/example/karanbatra/productiveninja/activity/SeeNotes.java
@@ -1,22 +1,30 @@
 package com.example.karanbatra.productiveninja.activity;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.karanbatra.productiveninja.R;
 
-public class SeeNotes extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class SeeNotes extends AppCompatActivity {
+    ArrayAdapter<String> adapter;
+    ArrayList<String> notes = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_see_notes);
-
-
+        final NotesDB db = new NotesDB(this);
+        List<NotesFields> contacts = db.getAllContacts();
+        for (NotesFields cn : contacts) {
+            String log = "Id: " + cn.get_id() + " ,Name: " + cn.get_name()   +", contents: "+cn.get_contents() + " deletevar :" + cn.get_delelte_var() + "\n";
+            notes.add(cn.get_name() + "\n" + cn.get_contents() + "\n");
+        }
+        adapter = new ArrayAdapter<>(this, R.layout.notes_list_item, R.id.notes_item, notes);
+        ListView listView = (ListView)findViewById(R.id.notes_list_view);
+        listView.setAdapter(adapter);
     }
-
 }
